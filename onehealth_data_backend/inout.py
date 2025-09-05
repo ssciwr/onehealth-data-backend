6544b4cf6164eb1b6e8bf7085589f2954a32360d
tuyenle <lethikimtuyen87@gmail.com>
2025-09-05 15:29:37 +0200

    move sub functions outside, fix sonarcloud issue

--- onehealth_data_backend/inout.py
+++ onehealth_data_backend/inout.py
@@ -46,6 +46,205 @@ def save_to_netcdf(data: xr.DataArray, filename: str, encoding: Dict = None):
     print("Data saved to {}".format(filename))
 
 
+def _format_ymdt(numbers: List[str] | None) -> str:
+    """Format years, months, days or times into a string representation.
+    If numbers is None, return empty string.
+    If numbers are continuous, return start and end values (e.g., '01_12').
+    If more than 5 numbers, return first 5 numbers followed by '_etc'.
+    Otherwise, return all numbers joined by underscores, each formatted as two digits.
+
+    Args:
+        numbers (List[str] | None): List of years, months, days, or times
+
+    Returns:
+        str: Formatted string representation.
+    """
+    if numbers is None or len(numbers) == 0:
+        return ""
+
+    num_list = sorted(int(num) for num in numbers)
+    are_continuous = (len(num_list) == (max(num_list) - min(num_list) + 1)) and (
+        len(num_list) > 1
+    )
+    if are_continuous:
+        return f"{min(num_list):02d}-{max(num_list):02d}"
+    elif len(num_list) > 5:
+        return "_".join(f"{n:02d}" for n in num_list[:5]) + "_etc"
+    else:
+        return "_".join(f"{n:02d}" for n in num_list)
+
+
+def _format_months(months: List[str] | None) -> str:
+    """Format months into a string representation.
+    If months is None, return empty string.
+    If all months are selected, return 'allm'.
+    If months are continuous, return start and end values.
+    If more than 5 months, return first 5 months followed by '_etc'.
+    Otherwise, return all months joined by underscores.
+
+    Args:
+        months (List[str] | None): List of months.
+
+    Returns:
+        str: Formatted string representation.
+    """
+    if months is None:
+        return ""
+    elif len(set(months)) == 12:
+        return "allm"
+    else:
+        return _format_ymdt(months)
+
+
+def _format_days(days: List[str] | None) -> str:
+    """Format days into a string representation.
+    If days is None, return empty string.
+    If all days are selected, return 'alld'.
+    If days are continuous, return start and end values.
+    If more than 5 days, return first 5 days followed by '_etc'.
+    Otherwise, return all days joined by underscores.
+
+    Args:
+        days (List[str] | None): List of days.
+
+    Returns:
+        str: Formatted string representation.
+    """
+    if days is None:
+        return ""
+    elif len(set(days)) == 31:  # TODO how about month with 30 or 28 days?
+        return "alld"
+    else:
+        return _format_ymdt(days)
+
+
+def _format_times(times: List[str] | None) -> str:
+    """Format times into a string representation.
+    If times is None, return empty string.
+    If only "00:00" is selected, return "midnight".
+    If all times are selected, return 'allt'.
+    If more than 5 times, return first 5 times followed by '_etc'.
+    Otherwise, return all times in two-digit format joined by underscores.
+
+    Args:
+        times (List[str] | None): List of times in "HH:MM" format
+
+    Returns:
+        str: Formatted string representation.
+    """
+    if times is None:
+        return ""
+    elif "00:00" in times and len(times) == 1:
+        return "midnight"
+    elif len(set(times)) == 24:
+        return "allt"
+    else:
+        return _format_ymdt([t.split(":")[0] for t in times])
+
+
+def _format_variables(variables: List[str]) -> str:
+    """Format variables into a string representation.
+    Join all first letters of variable names with underscores.
+    E.g. ['2m_temperature', 'total_precipitation'] -> '2t_tp'.
+    If the total length exceeds 30 characters,
+        limit to first 2 characters then add '_etc'.
+
+    Args:
+        variables (List[str]): List of variable names.
+
+    Returns:
+        str: Formatted string representation.
+    """
+    var_str = "_".join(
+        ["".join(word[0] for word in var.split("_")) for var in variables]
+    )
+    if len(var_str) > 30:
+        var_str = var_str[:2] + "_etc"  # e.g. 2t_etc
+    return var_str
+
+
+def _format_ds_type(ds_name: str, time_str: str) -> str:
+    """Format dataset type based on dataset name.
+    If 'monthly' is in the dataset name, return 'monthly'.
+    if 'monthly' is not in the dataset name and time_str is '_midnight',
+        return 'daily'.
+    Otherwise, return empty string.
+
+    Args:
+        ds_name (str): Dataset name.
+        time_str (str): Formatted time string.
+
+    Returns:
+        str: Formatted dataset type.
+    """
+    if "monthly" in ds_name:
+        return "monthly"
+    elif time_str == "_midnight":
+        return "daily"
+    else:
+        return ""
+
+
+def _file_extension(data_format: str) -> str:
+    """Get file extension based on data format.
+    If data format is 'grib', return 'grib'.
+    If data format is 'netcdf', return 'nc'.
+    Otherwise, raise ValueError.
+
+    Args:
+        data_format (str): Data format (e.g., "netcdf", "grib").
+
+    Returns:
+        str: File extension.
+    """
+    if data_format == "grib":
+        return "grib"
+    elif data_format == "netcdf":
+        return "nc"
+    else:
+        raise ValueError(f"Unsupported data format: {data_format}")
+
+
+def _area_format(has_area: bool) -> str:
+    """Get area string based on has_area flag.
+    If has_area is True, return 'area'.
+    Otherwise, return empty string.
+
+    Args:
+        has_area (bool): Flag indicating if area is included.
+
+    Returns:
+        str: Area string.
+    """
+    return "area" if has_area else ""
+
+
+def _add_prefix_if_not_empty(s: str, prefix: str = "_") -> str:
+    """Prefix a string with a given prefix if the string is not empty.
+
+    Args:
+        s (str): The input string.
+        prefix (str): The prefix to add. Default is "_".
+
+    Returns:
+        str: The prefixed string if not empty, otherwise an empty string.
+    """
+    return f"{prefix}{s}" if s else ""
+
+
+def _truncate_string(s: str, max_length: int = 100) -> str:
+    """Truncate a string to a maximum length, adding '_etc' if truncated.
+
+    Args:
+        s (str): The input string.
+        max_length (int): The maximum allowed length of the string. Default is 100.
+
+    Returns:
+        str: The truncated string if it exceeds max_length, otherwise the original string.
+    """
+    return s if len(s) <= max_length else s[:max_length] + "_etc"
+
+
 def get_filename(
     ds_name: str,
     data_format: str,
@@ -75,144 +274,22 @@ def get_filename(
     Returns:
         str: Generated file name.
     """
-
-    def format_ymdt(numbers: List[str] | None) -> str:
-        """Format years, months, days or times into a string representation.
-        If numbers is None, return empty string.
-        If numbers are continuous, return start and end values (e.g., '01_12').
-        If more than 5 numbers, return first 5 numbers followed by '_etc'.
-        Otherwise, return all numbers joined by underscores, each formatted as two digits.
-        """
-        if numbers is None:
-            return ""
-
-        num_list = sorted(int(num) for num in numbers)
-        are_continuous = (len(num_list) == (max(num_list) - min(num_list) + 1)) and (
-            len(num_list) > 1
-        )
-        if are_continuous:
-            return f"{min(num_list):02d}-{max(num_list):02d}"
-        elif len(num_list) > 5:
-            return "_".join(f"{n:02d}" for n in num_list[:5]) + "_etc"
-        else:
-            return "_".join(f"{n:02d}" for n in num_list)
-
-    def format_months(months: List[str] | None) -> str:
-        """Format months into a string representation.
-        If months is None, return empty string.
-        If all months are selected, return 'allm'.
-        If months are continuous, return start and end values.
-        If more than 5 months, return first 5 months followed by '_etc'.
-        Otherwise, return all months joined by underscores.
-        """
-        if months is None:
-            return ""
-        elif len(set(months)) == 12:
-            return "allm"
-        else:
-            return format_ymdt(months)
-
-    def format_days(days: List[str] | None) -> str:
-        """Format days into a string representation.
-        If days is None, return empty string.
-        If all days are selected, return 'alld'.
-        If days are continuous, return start and end values.
-        If more than 5 days, return first 5 days followed by '_etc'.
-        Otherwise, return all days joined by underscores.
-        """
-        if days is None:
-            return ""
-        elif len(set(days)) == 31:  # TODO how about month with 30 or 28 days?
-            return "alld"
-        else:
-            return format_ymdt(days)
-
-    def format_times(times: List[str] | None) -> str:
-        """Format times into a string representation.
-        If times is None, return empty string.
-        If only "00:00" is selected, return "midnight".
-        If all times are selected, return 'allt'.
-        If more than 5 times, return first 5 times followed by '_etc'.
-        Otherwise, return all times in two-digit format joined by underscores.
-        """
-        if times is None:
-            return ""
-        elif "00:00" in times and len(times) == 1:
-            return "midnight"
-        elif len(set(times)) == 24:
-            return "allt"
-        else:
-            return format_ymdt([t.split(":")[0] for t in times])
-
-    def format_variables(variables: List[str]) -> str:
-        """Format variables into a string representation.
-        Join all first letters of variable names with underscores.
-        E.g. ['2m_temperature', 'total_precipitation'] -> '2t_tp'.
-        If the total length exceeds 30 characters,
-            limit to first 2 characters then add '_etc'.
-        """
-        var_str = "_".join(
-            ["".join(word[0] for word in var.split("_")) for var in variables]
-        )
-        if len(var_str) > 30:
-            var_str = var_str[:2] + "_etc"  # e.g. 2t_etc
-        return var_str
-
-    def format_ds_type(ds_name: str, time_str: str) -> str:
-        """Format dataset type based on dataset name.
-        If 'monthly' is in the dataset name, return 'monthly'.
-        if 'monthly' is not in the dataset name and time_str is '_midnight',
-            return 'daily'.
-        Otherwise, return empty string.
-        """
-        if "monthly" in ds_name:
-            return "monthly"
-        elif time_str == "_midnight":
-            return "daily"
-        else:
-            return ""
-
-    def file_extension(data_format: str) -> str:
-        """Get file extension based on data format.
-        If data format is 'grib', return 'grib'.
-        Otherwise, return 'nc'.
-        """
-        if data_format == "grib":
-            return "grib"
-        else:
-            return "nc"
-
-    def area_format(has_area: bool) -> str:
-        """Get area string based on has_area flag.
-        If has_area is True, return 'area'.
-        Otherwise, return empty string.
-        """
-        return "area" if has_area else ""
-
-    def add_prefix_if_not_empty(s: str, prefix: str = "_") -> str:
-        """Prefix a string with a given prefix if the string is not empty."""
-        return f"{prefix}{s}" if s else ""
-
-    def truncate_string(s: str, max_length: int = 100) -> str:
-        """Truncate a string to a maximum length, adding '_etc' if truncated."""
-        return s if len(s) <= max_length else s[:max_length] + "_etc"
-
-    year_str = add_prefix_if_not_empty(format_ymdt(years))
-    month_str = add_prefix_if_not_empty(format_months(months))
-    day_str = add_prefix_if_not_empty(format_days(days))
-    time_str = add_prefix_if_not_empty(format_times(times))
-    var_str = add_prefix_if_not_empty(format_variables(variables))
-    ds_type = add_prefix_if_not_empty(format_ds_type(ds_name, time_str))
-    area_str = add_prefix_if_not_empty(area_format(has_area))
+    year_str = _add_prefix_if_not_empty(_format_ymdt(years))
+    month_str = _add_prefix_if_not_empty(_format_months(months))
+    day_str = _add_prefix_if_not_empty(_format_days(days))
+    time_str = _add_prefix_if_not_empty(_format_times(times))
+    var_str = _add_prefix_if_not_empty(_format_variables(variables))
+    ds_type = _add_prefix_if_not_empty(_format_ds_type(ds_name, time_str))
+    area_str = _add_prefix_if_not_empty(_area_format(has_area))
 
     file_name = f"{base_name}{year_str}{month_str}{day_str}{time_str}{var_str}{ds_type}{area_str}"
 
-    file_name = truncate_string(file_name, max_length=100)
+    file_name = _truncate_string(file_name, max_length=100)
 
     # add raw to file name
     file_name = file_name + "_raw"
 
-    file_ext = file_extension(data_format)
+    file_ext = _file_extension(data_format)
     file_name = file_name + "." + file_ext
 
     return file_name
--- onehealth_data_backend/test/test_inout.py
+++ onehealth_data_backend/test/test_inout.py
@@ -69,6 +69,169 @@ def test_save_to_netcdf(get_data, tmp_path):
     file_name.unlink()
 
 
+def test_format_ymdt():
+    numbers = None
+    assert inout._format_ymdt(numbers) == ""
+
+    numbers = []
+    assert inout._format_ymdt(numbers) == ""
+
+    numbers = [str(i).zfill(2) for i in range(1, 7)]
+    assert inout._format_ymdt(numbers) == "01-06"
+
+    numbers = [str(i).zfill(2) for i in range(1, 13, 2)]
+    assert inout._format_ymdt(numbers) == "01_03_05_07_09_etc"
+
+    numbers = [str(i).zfill(2) for i in range(1, 4, 2)]
+    assert inout._format_ymdt(numbers) == "01_03"
+
+
+def test_format_months():
+    months = None
+    assert inout._format_months(months) == ""
+
+    months = []
+    assert inout._format_months(months) == ""
+
+    months = [str(i).zfill(2) for i in range(1, 13)]
+    assert inout._format_months(months) == "allm"
+
+    months = [str(i).zfill(2) for i in range(1, 7)]
+    assert inout._format_months(months) == "01-06"
+
+    months = [str(i).zfill(2) for i in range(1, 13, 2)]
+    assert inout._format_months(months) == "01_03_05_07_09_etc"
+
+    months = [str(i).zfill(2) for i in range(1, 4, 2)]
+    assert inout._format_months(months) == "01_03"
+
+
+def test_format_days():
+    days = None
+    assert inout._format_days(days) == ""
+
+    days = []
+    assert inout._format_days(days) == ""
+
+    days = [str(i).zfill(2) for i in range(1, 32)]
+    assert inout._format_days(days) == "alld"
+
+    days = [str(i).zfill(2) for i in range(1, 16)]
+    assert inout._format_days(days) == "01-15"
+
+    days = [str(i).zfill(2) for i in range(1, 32, 2)]
+    assert inout._format_days(days) == "01_03_05_07_09_etc"
+
+    days = [str(i).zfill(2) for i in range(1, 4, 2)]
+    assert inout._format_days(days) == "01_03"
+
+
+def test_format_times():
+    times = None
+    assert inout._format_times(times) == ""
+
+    times = []
+    assert inout._format_times(times) == ""
+
+    times = ["00:00"]
+    assert inout._format_times(times) == "midnight"
+
+    times = [f"{str(i).zfill(2)}:00" for i in range(0, 24)]
+    assert inout._format_times(times) == "allt"
+
+    times = [f"{str(i).zfill(2)}:00" for i in range(0, 12)]
+    assert inout._format_times(times) == "00-11"
+
+    times = [f"{str(i).zfill(2)}:00" for i in range(0, 24, 2)]
+    assert inout._format_times(times) == "00_02_04_06_08_etc"
+
+    times = ["00:00", "02:00", "04:00"]
+    assert inout._format_times(times) == "00_02_04"
+
+
+def test_format_variables():
+    variables = []
+    assert inout._format_variables(variables) == ""
+
+    variables = ["2m_temperature"]
+    assert inout._format_variables(variables) == "2t"
+
+    variables = ["2m_temperature", "total_precipitation"]
+    assert inout._format_variables(variables) == "2t_tp"
+
+    variables = [
+        "2m_temperature",
+        "total_precipitation",
+        "surface_pressure",
+        "10m_u_component_of_wind",
+        "10m_v_component_of_wind",
+        "mean_sea_level_pressure",
+        "total_cloud_cover",
+        "low_cloud_cover",
+        "medium_cloud_cover",
+        "high_cloud_cover",
+    ]
+    assert inout._format_variables(variables) == "2t_etc"
+
+
+def test_format_ds_type():
+    ds_name = "reanalysis-era5-land-monthly-means"
+    time_str = "_midnight"
+    assert inout._format_ds_type(ds_name, time_str) == "monthly"
+
+    ds_name = "reanalysis-era5-land"
+    time_str = "_midnight"
+    assert inout._format_ds_type(ds_name, time_str) == "daily"
+
+    ds_name = "reanalysis-era5-land"
+    time_str = "_00-11"
+    assert inout._format_ds_type(ds_name, time_str) == ""
+
+
+def test_file_extension():
+    data_format = "netcdf"
+    assert inout._file_extension(data_format) == "nc"
+
+    data_format = "grib"
+    assert inout._file_extension(data_format) == "grib"
+
+    data_format = "unknown"
+    with pytest.raises(ValueError):
+        inout._file_extension(data_format)
+
+
+def test_area_format():
+    has_area = True
+    assert inout._area_format(has_area) == "area"
+
+    has_area = False
+    assert inout._area_format(has_area) == ""
+
+
+def test_add_prefix_if_not_empty():
+    prefix = "_"
+    string = "test"
+    assert inout._add_prefix_if_not_empty(string, prefix) == "_test"
+
+    prefix = "_"
+    string = ""
+    assert inout._add_prefix_if_not_empty(string, prefix) == ""
+
+
+def test_truncate_string():
+    string = "a" * 50
+    max_length = 2
+    assert inout._truncate_string(string, max_length) == "aa_etc"
+
+    string = "short_string"
+    max_length = 30
+    assert inout._truncate_string(string, max_length) == "short_string"
+
+    string = "a" * 100
+    max_length = 100
+    assert inout._truncate_string(string, max_length) == "a" * 100
+
+
 def test_get_filename_var():
     file_name = inout.get_filename(
         "reanalysis-era5-land-monthly-means",
